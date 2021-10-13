b99f7dbcdf1443402c1d9c70ac381ae01e7998bf
julianamrg <jumeroga1@gmail.com>
2021-10-13 18:02:40 -0500

    prueba 4

--- Project_API/urls.py
+++ Project_API/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 from django.config import settings
-from django.config.urls.static impor static
+from django.config.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
